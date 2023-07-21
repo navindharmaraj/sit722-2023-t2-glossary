1613e7067ecda12ab2e6a666b4e0b378eab73bd1
Navin Dharmarajan <navindharmaraj@users.noreply.github.com>
2023-07-21 15:16:34 +1000

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,13 @@ var router = express.Router();
 var glossaryService = require('../services/glossary');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   const glossaryData = glossaryService.getGlossary();
-  res.render('index', { title: 'SIT722 DevOps Glossary', subtitle:'Welcome to my website showing a collection of DevOps terms and their brief description.', data: glossaryData });
+  res.render('index', {
+    title: 'SIT722 DevOps Glossary',
+    subtitle: 'Welcome to my website showing a collection of DevOps terms and their brief description.',
+    data: glossaryData
+  });
 });
 
 module.exports = router;
